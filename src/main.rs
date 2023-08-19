da22a0983fcaea3c3ac779059ad86120699997a1
FaizBShah <faizbshah2001@gmail.com>
2023-08-19 19:32:44 +0530

    Formatted

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use std::cmp::Ordering;
 // Taking input from standard input and printing it
 fn main() {
     println!("What is your name?");
+
     let mut name= String::new();
     let greeting = "Nice to meet you";
+
     io::stdin().read_line(&mut name).expect("Didn't receive Input");
+    
     print!("Hello {}! {}", name.trim_end(), greeting);
 }
