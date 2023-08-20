77812d5e51c426c8bd71adf5138f2f63c215b213
FaizBShah <faizbshah2001@gmail.com>
2023-08-20 17:09:36 +0530

    random

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-// Math
+use rand::Rng;
+
+// Random
 fn main() {
-    let num_1: f32 = 1.111111111111111;
-    println!("f32: {}", num_1 + 0.111111111111111); // 1.2222223 (6 digits of precision)
-
-    let num_2: f64 = 1.111111111111111;
-    println!("f64: {}", num_2 + 0.111111111111111); // 1.2222222222222219 (14 digits of precision)
-
-    let num_3: u32 = 5;
-    let num_4: u32 = 4;
-
-    println!("5 + 4 = {}", num_3 + num_4);
-    println!("5 - 4 = {}", num_3 - num_4);
-    println!("5 * 4 = {}", num_3 * num_4);
-    println!("5 / 4 = {}", num_3 / num_4);
-    println!("5 % 4 = {}", num_3 % num_4);
+    let random_num = rand::thread_rng().gen_range(1..101); // Generates a random no. between 1 and 100
+    println!("Random number: {}", random_num);
 }
