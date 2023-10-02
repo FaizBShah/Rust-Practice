3c52d01b9a217bcfbc1c136548cb0b2cdcf9722b
FaizBShah <faizbshah2001@gmail.com>
2023-10-02 17:11:44 +0530

    Functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
-// Vectors
+// Functions
+
+fn say_hello() {
+    println!("Hello World");
+}
+
+fn get_sum(x: i32, y: i32) {
+    println!("{} + {} = {}", x, y, x + y);
+}
+
+fn get_sum2(x: i32, y: i32) -> i32 {
+    x + y // Equivalent to return x + y; Using semi-colon here is invalid
+}
+
+fn get_sum3(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+// To return multiple values, return a tuple (The parentheses are necessary)
+fn get_square_and_cube(x: i32) -> (i32, i32) {
+    return (x * x, x * x * x);
+}
+
+fn sum_list(list: &Vec<i32>) -> i32 {
+    let mut sum: i32 = 0;
+
+    for num in list  {
+        sum += num;
+    }
+
+    return sum;
+}
+
 fn main() {
-   let _vec1: Vec<i32> = Vec::new(); // Immutable vector
-   let mut vec2: Vec<i32> = vec![1, 2, 3, 4];
-   vec2.push(5);
-   println!("{}", vec2[0]);
+   say_hello();
+   get_sum(2, 3);
+   println!("{} + {} = {}", 3, 4, get_sum2(3, 4));
+   println!("{} + {} = {}", 6, 9, get_sum3(6, 9));
 
-   let _second: &i32 = &vec2[1];
+   let (square, cube) = get_square_and_cube(7);
 
-   match vec2.get(1) {
-       Some(value) => println!("{}", value),
-       None => println!("No second value")
-   }
-   
-   for i in &mut vec2 {
-       *i *= 2;
-   }
+   println!("The square and cube of {} is: {} and {}", 7, square, cube);
 
-   for i in &mut vec2 {
-        println!("{}", i);
-   }
-
-   println!("Length: {}", vec2.len());
-   println!("Pop: {:?}", vec2.pop());
+   let num_list = vec![1, 2, 3, 4, 5];
+   println!("Sum of list = {}", sum_list(&num_list));
 }
