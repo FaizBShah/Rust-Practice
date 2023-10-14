4b6df11405be2c85fe0b23cf3f3e68ce4a8c54e4
FaizBShah <faizbshah2001@gmail.com>
2023-10-14 20:31:29 +0530

    Traits

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
-// Structs
+// Traits
+use std::f32::consts::PI;
+
+trait Shape {
+    fn new(length: f32, width: f32) -> Self;
+    fn area(&self) -> f32;
+}
+
+struct Rectangle {
+     length: f32,
+     width: f32
+}
+
+struct Circle {
+     length: f32,
+     width: f32
+}
+
+// Implementing Shape trait for Rectangle struct
+impl Shape for Rectangle {
+     fn new(length: f32, width: f32) -> Rectangle {
+         Rectangle { length, width }
+     }
+
+     fn area(&self) -> f32 {
+         return self.length * self.width;
+     }
+}
+
+// Implementing Shape trait for Circle struct
+impl Shape for Circle {
+     fn new(length: f32, width: f32) -> Circle {
+         Circle { length, width }
+     }
+
+     fn area(&self) -> f32 {
+         return PI * self.length * self.width;
+     }
+ }
+
 fn main() {
-   struct Customer {
-        name: String,
-        address: String,
-        balance: f32
-   }
+    let rectangle: Rectangle = Rectangle::new(3.0, 4.0);
+    let circle: Circle = Circle::new(2.0, 2.0);
 
-   let mut bob = Customer {
-        name: String::from("Bob Marley"),
-        address: String::from("555 Main St."),
-        balance: 234.50
-   };
-
-   bob.balance = 123.20;
-
-   println!("{}, {} - {}", bob.name, bob.address, bob.balance);
-
-   // structs with generics
-   struct Rectangle<T, U> {
-        length: T,
-        width: U
-   }
-
-   let rec = Rectangle {
-        length: 1,
-        width: 2.4
-   };
-
-   println!("{} {}", rec.length, rec.width);
+    println!("Area of the rectangle and circle is {} and {}, respectively", rectangle.area(), circle.area());
 }
