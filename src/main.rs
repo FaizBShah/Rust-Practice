bd3446316a29d07b08a2542a8b44fd6975cb0dbf
FaizBShah <faizbshah2001@gmail.com>
2023-10-15 18:34:56 +0530

    Modules

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,8 @@
-// Traits
-use std::f32::consts::PI;
+// Modules
+mod restaurant;
 
-trait Shape {
-    fn new(length: f32, width: f32) -> Self;
-    fn area(&self) -> f32;
-}
-
-struct Rectangle {
-     length: f32,
-     width: f32
-}
-
-struct Circle {
-     length: f32,
-     width: f32
-}
-
-// Implementing Shape trait for Rectangle struct
-impl Shape for Rectangle {
-     fn new(length: f32, width: f32) -> Rectangle {
-         Rectangle { length, width }
-     }
-
-     fn area(&self) -> f32 {
-         return self.length * self.width;
-     }
-}
-
-// Implementing Shape trait for Circle struct
-impl Shape for Circle {
-     fn new(length: f32, width: f32) -> Circle {
-         Circle { length, width }
-     }
-
-     fn area(&self) -> f32 {
-         return PI * self.length * self.width;
-     }
- }
+use restaurant::order_food;
 
 fn main() {
-    let rectangle: Rectangle = Rectangle::new(3.0, 4.0);
-    let circle: Circle = Circle::new(2.0, 2.0);
-
-    println!("Area of the rectangle and circle is {} and {}, respectively", rectangle.area(), circle.area());
+    order_food();
 }
