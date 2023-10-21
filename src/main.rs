b4d35630c165a25701a78e6814ed8b7d1f48c2b0
FaizBShah <faizbshah2001@gmail.com>
2023-10-21 21:18:25 +0530

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::io::ErrorKind;
-use std::fs::File;
-
-// ErrorKind
-
+// Iterators
 fn main() {
-    let path = "rand.txt";
-    match File::open(path) {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(path) {
-                Ok(file) => file,
-                Err(error) => panic!("Cannot create file: {:?}", error)
-            },
-            _other_error => panic!("Cannot open the file: {:?}", _other_error)
-        }
-    };
+    let mut arr = [1, 2, 3, 4];
+
+    for val in arr.iter() {
+        println!("{}", val); // Can read the values
+    }
+
+    for val in arr.iter_mut() {
+        *val *= 2; // Can update the value
+    }
+
+    println!("{:?}", arr);
+
+    let mut iter = arr.iter();
+
+    println!("{:?}", iter.next()); // Some(2)
+    println!("{:?}", iter.next()); // Some(4)
+    println!("{:?}", iter.next()); // Some(6)
+    println!("{:?}", iter.next()); // Some(8)
+    println!("{:?}", iter.next()); // None
 }
