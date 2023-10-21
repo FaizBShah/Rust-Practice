4207d1d3f772fafdc21e72ad473bf61dbecd2ae5
FaizBShah <faizbshah2001@gmail.com>
2023-10-21 20:45:50 +0530

    File handling and result

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-// Error handling
+use std::io::{Write, BufReader, BufRead};
+use std::fs::File;
+
+// File handling and Result
+
+// Result has 2 variants Ok and Err
+// enum Result<T, E> {
+//    Ok(T),
+//    Err(E),
+// }
+// Where T represents the datatype of the return value, and E the type of error
+
 fn main() {
-    // panic!("Terrible Error"); // To create an unexpected error and quit
-    let lil_arr = [1, 2];
-    // println!("{}", lil_arr[10]); // Will throw an error
+    let path = "lines.txt";
+    let mut output = match File::create(path) {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file: {:?}",  error);
+        }
+    };
+
+    write!(output, "Just Some\nRandom words").expect("Failed to write to file"); // If any error happens, then throw this message
+
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
 }
