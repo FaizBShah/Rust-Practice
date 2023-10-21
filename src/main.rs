21740cc0b84b32b1d2648b92d96ffd9c005d8664
FaizBShah <faizbshah2001@gmail.com>
2023-10-21 20:57:39 +0530

    ErrorKind

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
-use std::io::{Write, BufReader, BufRead};
+use std::io::ErrorKind;
 use std::fs::File;
 
-// File handling and Result
-
-// Result has 2 variants Ok and Err
-// enum Result<T, E> {
-//    Ok(T),
-//    Err(E),
-// }
-// Where T represents the datatype of the return value, and E the type of error
+// ErrorKind
 
 fn main() {
-    let path = "lines.txt";
-    let mut output = match File::create(path) {
+    let path = "rand.txt";
+    match File::open(path) {
         Ok(file) => file,
-        Err(error) => {
-            panic!("Problem creating file: {:?}",  error);
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(file) => file,
+                Err(error) => panic!("Cannot create file: {:?}", error)
+            },
+            _other_error => panic!("Cannot open the file: {:?}", _other_error)
         }
     };
-
-    write!(output, "Just Some\nRandom words").expect("Failed to write to file"); // If any error happens, then throw this message
-
-    let input = File::open(path).unwrap();
-    let buffered = BufReader::new(input);
-
-    for line in buffered.lines() {
-        println!("{}", line.unwrap());
-    }
 }
