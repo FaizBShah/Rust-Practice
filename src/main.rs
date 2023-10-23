f89658c63b9fa382dd6406c9e6ad219b381403c2
FaizBShah <faizbshah2001@gmail.com>
2023-10-23 16:45:50 +0530

    Smart Pointers

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-// Closures
+// Smart Pointers
 fn main() {
-    let can_vote = |age: i32| {
-        age >= 18
-    };
-    println!("Can vote: {}", can_vote(22));
+    let b_int1 = Box::new(10);
+    println!("{}", b_int1);
 
-    let mut samp1 = 5;
-    let print_var = || println!("samp1 = {}", samp1);
-    print_var();
-    samp1 = 10;
-    let mut change_var = || samp1 += 1;
-    change_var();
-    println!("samp1 = {}", samp1);
-    samp1 = 10;
-    println!("samp1 = {}", samp1);
-
-    fn use_func<T>(a: i32, b: i32, func: T) -> i32 where T: Fn(i32, i32) -> i32 {
-        func(a, b)
+    struct TreeNode<T> {
+        pub key: T,
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>
     }
 
-    let sum = |a: i32, b: i32| a + b;
-    let prod = |a: i32, b: i32| a * b;
+    impl<T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode { key, left: None, right: None }
+        }
 
-    println!("{} + {} = {}", 2, 3, use_func(2, 3, sum));
-    println!("{} * {} = {}", 2, 3, use_func(2, 3, prod));
+        pub fn left(mut self, node: TreeNode<T>) -> Self {
+            self.left = Some(Box::new(node));
+            self
+        }
+
+        pub fn right(mut self, node: TreeNode<T>) -> Self {
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+
+    let root = TreeNode::new(1).left(TreeNode::new(2)).right(TreeNode::new(3));
+
+    println!("{}", root.key);
 }
