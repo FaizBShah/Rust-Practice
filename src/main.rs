065a3c8958cd07b7b8b6f16babd694bb53caee94
FaizBShah <faizbshah2001@gmail.com>
2023-10-24 20:26:51 +0530

    Threads

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,78 @@
-// Smart Pointers
+// Concurrency
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
+
+pub struct Bank {
+    balance: f32
+}
+
+// fn withdraw(bank: &mut Bank, amt: f32) {
+//     bank.balance -= amt;
+// }
+
+// fn customer(bank: &mut Bank) {
+//     withdraw(bank, 5.0);
+// }
+
+fn withdraw(bank: &Arc<Mutex<Bank>>, amt: f32) {
+    let mut bank_ref = bank.lock().unwrap();
+
+    if bank_ref.balance < 5.00 {
+        println!("Current balance: {}. Withdraw a smaller amount", bank_ref.balance);
+    } else {
+        bank_ref.balance -= amt;
+        println!("Customer withdrew: {}. Current balance: {}", amt, bank_ref.balance);
+    }
+}
+
+fn customer(bank: Arc<Mutex<Bank>>) {
+    withdraw(&bank, 5.0);
+}
+
 fn main() {
-    let b_int1 = Box::new(10);
-    println!("{}", b_int1);
+    // let thread1 = thread::spawn(|| {
+    //     for i in 1..25 {
+    //         println!("Spawned thread: {}", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
 
-    struct TreeNode<T> {
-        pub key: T,
-        pub left: Option<Box<TreeNode<T>>>,
-        pub right: Option<Box<TreeNode<T>>>
+    // for i in 1..20 {
+    //     println!("Main thread: {}", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // // Waits for the thread1 to finish even if the main thread finishes
+    // thread1.join().unwrap();
+
+    // let mut bank = Bank { 
+    //     balance: 100.0 
+    // };
+
+    // withdraw(&mut bank, 5.0);
+
+    // This is giving error as bank cannot be borrowed from main function
+    // thread::spawn(|| {
+    //     customer(&mut bank);
+    // }).join().unwrap();
+
+    // println!("Balance: {}", bank.balance);
+
+    let bank = Arc::new(Mutex::new(Bank { balance: 20.0 }));
+
+    let handles = (0..10).map(|_| {
+        let bank_ref = bank.clone();
+        thread::spawn(|| {
+            customer(bank_ref);
+        })
+    });
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    impl<T> TreeNode<T> {
-        pub fn new(key: T) -> Self {
-            TreeNode { key, left: None, right: None }
-        }
-
-        pub fn left(mut self, node: TreeNode<T>) -> Self {
-            self.left = Some(Box::new(node));
-            self
-        }
-
-        pub fn right(mut self, node: TreeNode<T>) -> Self {
-            self.right = Some(Box::new(node));
-            self
-        }
-    }
-
-    let root = TreeNode::new(1).left(TreeNode::new(2)).right(TreeNode::new(3));
-
-    println!("{}", root.key);
+    println!("Total balance: {}", bank.lock().unwrap().balance);
 }
